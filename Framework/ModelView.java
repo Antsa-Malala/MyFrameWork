f4f5c541207f49d603fe9d742c2bb6970f25aa86
Antsa-Malala <119752846+Antsa-Malala@users.noreply.github.com>
2023-06-13 22:01:06 +0300

    sprint11

--- Framework/ModelView.java
+++ Framework/ModelView.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 public class ModelView {
     private String view;
     private HashMap<String,Object> data;
+    private HashMap<String,Object> session;
 
     public String getview()
     {
@@ -17,10 +18,19 @@ public class ModelView {
     {
         this.data=data;
     }
+    public void setsession(HashMap<String,Object> session)
+    {
+        this.session=session;
+    }
 
     public HashMap<String,Object> getdata()
     {
         return this.data;
+    }
+    
+    public HashMap<String,Object> getsession()
+    {
+        return this.session;
     }
 
     public void addItem(String cle,Object value)
@@ -28,9 +38,14 @@ public class ModelView {
         this.data.put(cle,value);
     }
 
+    public void addSession(String cle,Object value)
+    {
+        this.session.put(cle,value);
+    }
 
     public ModelView()
     {
         this.setdata(new HashMap<String,Object>());
+        this.setsession(new HashMap<String,Object>());
     }
 }
